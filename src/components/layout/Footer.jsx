1dd6c86abefba6d075e405cad5f6df0df1cb4316
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2023-12-04 16:10:08 +0100

    footer section

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,37 @@
+import "../../styles/css/footer.css";
+import xLogo from "../../assets/icons/x.svg";
+import gitHub from "../../assets/icons/github.svg";
+import globe from "../../assets/icons/globe.svg";
+import discord from "../../assets/icons/discord.svg";
+
 function Footer() {
-  return <></>;
+  const year = new Date().getFullYear();
+
+  return (
+    <>
+      <footer className="footer">
+        <div className="footer-credits">
+          Copyright &copy;{year} All rights reserved. Made by{" "}
+          <span>Unnamed</span>, <span>Cypheir</span> and many others with love.
+          💙
+        </div>
+        <div className="footer-social">
+          <a href="#" className="social-icon">
+            <img src={xLogo} alt="X/Twitter" />
+          </a>
+          <a href="#" className="social-icon">
+            <img src={discord} alt="Discord" />
+          </a>
+          <a href="#" className="social-icon">
+            <img src={gitHub} alt="GitHub" />
+          </a>
+          <a href="#" className="social-icon">
+            <img src={globe} alt="Web" />
+          </a>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
